59c18e42b42c8d4f1e36bf2a0d8e43771780d061
Salvatore Tedde <microcipcip@gmail.com>
2019-01-03 13:11:02 +0000

    Adding BpInit

---
+++ src/lib/core.ts
@@ -0,0 +1,99 @@
+import { css } from 'styled-components'
+import { ArrayType } from 'typedoc/dist/lib/models'
+
+const bpDefaults = {
+  xxxs: 0,
+  xxs: 200,
+  xs: 320,
+  s: 400,
+  sl: 500,
+  sl2: 580,
+  m: 768,
+  ml: 992,
+  l: 1100,
+  xl: 1200,
+  xxl: 1300,
+  xxxl: 1400,
+  xxxxl: 1500,
+  xxxxxl: 1600,
+  xxxxxxl: 1700,
+}
+
+type Tbp = {
+  [bp: string]: number
+}
+
+const getBpValue = (
+  val: number | string | null | undefined,
+  bp: Tbp
+): number => {
+  switch (typeof val) {
+    case 'number':
+      return val
+    case 'string':
+      if (!bp.hasOwnProperty(val)) {
+        throw `Breakpoint error: oops, you passed a value that is not defined in the 'bp' object.`
+      }
+      if (typeof bp[val] !== 'number') {
+        throw 'Breakpoint error: oops, you passed a value that is not a proper number.'
+      }
+
+      return bp[val]
+    default:
+      return -1
+  }
+}
+
+/**
+ * Initialize breakpoints
+ *
+ * ### Usage
+ * ```js
+ * import { css } from 'styled-components'
+ * import BpInit from 'styled-components-mq'
+ *
+ * // initialize
+ * const bpList = {s: 400, sl: 500, m: 768, ml: 992, l: 1100}
+ * const bp = BpInit({ bp: bpList })
+ * ```
+ *
+ * @param bp      Pass a breakpoint object to customize the breakpoints you wish to use.
+ * @param type    Pass a type string to either use min/max-width or min/max-height.
+ * @returns       A styled-component css string.
+ */
+
+const BpInit = ({ bp = bpDefaults, type = 'width' } = {}) => (
+  min?: string | number | null,
+  max?: string | number | null
+) => (contentCSS: ArrayType) => {
+  const minV = getBpValue(min, bp)
+  const maxV = getBpValue(max, bp)
+
+  if (!Array.isArray(contentCSS)) {
+    throw `Breakpoint error: oops, you passed a string instead of the styled-component css\`\` helper.`
+  }
+
+  if (minV >= 0 && !maxV) {
+    return css`
+      @media only screen and (min-${type}: ${minV}px) {
+        ${contentCSS}
+      }
+    `
+  } else if (!minV && maxV >= 0) {
+    return css`
+      @media only screen and (max-${type}: ${maxV - 1}px) {
+        ${contentCSS}
+      }
+    `
+  } else if (minV >= 0 && maxV >= 0) {
+    return css`
+      @media only screen and (min-${type}: ${minV}px) and (max-${type}: ${maxV -
+      1}px) {
+        ${contentCSS}
+      }
+    `
+  }
+  return css``
+}
+
+export default BpInit
